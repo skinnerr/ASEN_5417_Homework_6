9b9a08bce0e9ce3fbdc0c6531ad7af63003b0b17
Ryan Skinner <anomaly1452@gmail.com>
2015-11-10 21:51:13 -0700

    Show instability from SOR with half-domain.

--- matlab_code/Problem_2.m
+++ matlab_code/Problem_2.m
@@ -14,23 +14,24 @@ function [] = Problem_2()
     %%%
     
     % Solution domain: the closed interval [0,1]x[0,1]. Assume dx = dy = h.
-    N = 101;
-    x = linspace(0,1,N);
-    y = linspace(0,1,N);
-    h = x(2) - x(1);
+    N = 51;
+    M = 101;
+    x = linspace(0,1/2,N);
+    y = linspace(0,1  ,M);
+    h = y(2) - y(1);
     
     % Source term.
     xi = -1;
     
     % Boundary conditions (u and uprime) defined as cardinal directions (n, s, w, e).
     BC.us = 0;
-    BC.uw = 0;
-    BC.ue = 0;
     BC.un = 1;
+    BC.uw = 0;
+    BC.upe = 0;
     
     % Relaxation parameters to test.
     omega = [linspace(1.7,1.8,21), linspace(1.8,1.999,61)];
-    omega = 1.3;
+    omega = 1.4;
     
     n_to_converge = nan(length(omega));
     
@@ -43,11 +44,10 @@ function [] = Problem_2()
         fprintf('Working on omega = %6.3f\n', om);
     
         % Initialize the solution, indexed by (x,y), and set BCs.
-        u = zeros(N,N);
+        u = zeros(N,M);
         u(:,1)   = BC.us;
         u(:,end) = BC.un;
         u(1,:)   = BC.uw;
-        u(end,:) = BC.ue;
     
         % Solution norm.
         epsilon = inf;
@@ -66,10 +66,16 @@ function [] = Problem_2()
             for i = 2:N-1
                 % Within each column, loop over rows.
                 u_cols = u;
-                for j = 2:N-1
-                    u(i,j) = (1/4) * (   u_cols(i-1, j) + u_cols(i, j-1) ...
-                                       + u_cols(i+1, j) + u_cols(i, j+1) ...
-                                       - h^2 * xi                        );
+                for j = 2:M-1
+                    if i < N-1
+                        u(i,j) = (1/4) * (   u_cols(i-1, j) + u_cols(i, j-1) ...
+                                           + u_cols(i+1, j) + u_cols(i, j+1) ...
+                                           - h^2 * xi                        );
+                    else
+                        u(i,j) = (1/4) * (   u_cols(i-1, j) + u_cols(i, j-1) ...
+                                           + u_cols(i  , j) + u_cols(i, j+1) ...
+                                           - h^2 * xi                        );
+                    end
 %                     u(i,j) = (1 - omega) * u_cols(i,j) ...
 %                              + (omega/4) * (   u_cols(i-1, j) + u_cols(i, j-1) ...
 %                                              + u_cols(i+1, j) + u_cols(i, j+1) ...
@@ -78,6 +84,7 @@ function [] = Problem_2()
 %                 u(i,:) = omega * u(i,:) + (1 - omega) * u_cols(i,:);
                 
             end
+            u(end,:) = u(end-1,:);
             u = omega * u + (1 - omega) * u_prev;
     
             surf(x,y,u');
